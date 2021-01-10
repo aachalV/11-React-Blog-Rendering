b4073540171ca72004a84ee7c41624e3873e0c43
aachalv <aachal.vartak@raweng.com>
2021-01-10 23:20:44 +0530

    Added Navigation component and did styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import Home from "../src/pages/home/Home";
+import Navigation from "./components/navigation/Navigation";
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Home />
     </div>
   );
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import styles from "../navigation/Navigation.module.css";
+class Navigation extends Component {
+  render() {
+    return (
+      <div id="nav" className={styles["navbar"]}>
+        <div>
+          <img
+            src="https://www.raweng.com/v3/assets/bltaacb6b0c9b693c2d/bltc1a71705f49d391d/5de663548d1dce6ad6bed8bf/header-logo.svg?format=pjpg&width=220"
+            alt=""
+            className={styles["logo"]}
+          />
+        </div>
+        <div className={styles["nav-links"]}>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </div>
+    );
+  }
+}
+export default Navigation;
