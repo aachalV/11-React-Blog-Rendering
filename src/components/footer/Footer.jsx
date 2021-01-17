ac29fa81b67054df189b9162af4237579f7a12cb
aachalv <aachal.vartak@raweng.com>
2021-01-17 13:19:02 +0530

    Fixed warnings for footer component by adding href, target, and rel attributes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,12 +8,29 @@ function Footer() {
         © 2020 All Rights Reserved by <span>Aachal Vartak</span>
       </p>
       <div className={styles["social-links"]}>
-        <a href="#" class="fa fa-twitter twitter"></a>|
-        <a href="#" class="fa fa-linkedin linkedin"></a>|
-        <a href="#" class="fa fa-facebook facebook"></a>|
-        <a href="#" class="fa fa-github github"></a>
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noreferrer"
+          class="fa fa-twitter twitter"
+        ></a>
+        |
+        <a
+          href="https://www.linkedin.com/"
+          target="_blank"
+          rel="noreferrer"
+          class="fa fa-linkedin linkedin"
+        ></a>
+        |
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noreferrer"
+          class="fa fa-facebook facebook"
+        ></a>
+        |<a href="https://github.com/" class="fa fa-github github"></a>
       </div>
-      <a id="upArrow" href="#nav">
+      <a id="upArrow" href="#nav" target="_blank" rel="noreferrer">
         <i class="fa fa-arrow-circle-up"></i>
       </a>
     </footer>
